6d86671d4140f4885c4ee709ed7d3c75183853da
Alexander Piskun <bigcat88@icloud.com>
2023-12-17 16:31:09 +0300

    initial commit

---
+++ Makefile
@@ -0,0 +1,65 @@
+.DEFAULT_GOAL := help
+
+.PHONY: help
+help:
+	@echo "Welcome to AIImageGeneratorBot example. Please use \`make <target>\` where <target> is one of"
+	@echo " "
+	@echo "  Next commands are only for dev environment with nextcloud-docker-dev!"
+	@echo "  They should run from the host you are developing on(with activated venv) and not in the container with Nextcloud!"
+	@echo "  "
+	@echo "  build-push        build image and upload to ghcr.io"
+	@echo "  "
+	@echo "  deploy            deploy example to registered 'docker_dev' for Nextcloud Last"
+	@echo "  deploy27          deploy example to registered 'docker_dev' for Nextcloud 27"
+	@echo "  "
+	@echo "  run               install AIImageGeneratorBot for Nextcloud Last"
+	@echo "  run27             install AIImageGeneratorBot for Nextcloud 27"
+	@echo "  "
+	@echo "  For development of this example use PyCharm run configurations. Development is always set for last Nextcloud."
+	@echo "  First run 'AIImageGeneratorBot' and then 'make registerXX', after that you can use/debug/develop it and easy test."
+	@echo "  "
+	@echo "  register          perform registration of running 'AIImageGeneratorBot' into the 'manual_install' deploy daemon."
+	@echo "  register27        perform registration of running 'AIImageGeneratorBot' into the 'manual_install' deploy daemon."
+
+.PHONY: build-push
+build-push:
+	docker login ghcr.io
+	docker buildx build --push --platform linux/arm64/v8,linux/amd64 --tag ghcr.io/cloud-py-api/ai_image_generator_bot:latest .
+
+.PHONY: deploy
+deploy:
+	docker exec master-nextcloud-1 sudo -u www-data php occ app_api:app:unregister ai_image_generator_bot --silent || true
+	docker exec master-nextcloud-1 sudo -u www-data php occ app_api:app:deploy ai_image_generator_bot docker_dev \
+		--info-xml https://raw.githubusercontent.com/cloud-py-api/ai_image_generator_bot/main/appinfo/info.xml
+
+.PHONY: deploy27
+deploy27:
+	docker exec master-stable27-1 sudo -u www-data php occ app_api:app:unregister ai_image_generator_bot --silent || true
+	docker exec master-stable27-1 sudo -u www-data php occ app_api:app:deploy ai_image_generator_bot docker_dev \
+		--info-xml https://raw.githubusercontent.com/cloud-py-api/ai_image_generator_bot/main/appinfo/info.xml
+
+.PHONY: run
+run:
+	docker exec master-nextcloud-1 sudo -u www-data php occ app_api:app:unregister ai_image_generator_bot --silent || true
+	docker exec master-nextcloud-1 sudo -u www-data php occ app_api:app:register ai_image_generator_bot docker_dev --force-scopes \
+		--info-xml https://raw.githubusercontent.com/cloud-py-api/ai_image_generator_bot/main/appinfo/info.xml
+
+.PHONY: run27
+run27:
+	docker exec master-stable27-1 sudo -u www-data php occ app_api:app:unregister ai_image_generator_bot --silent || true
+	docker exec master-stable27-1 sudo -u www-data php occ app_api:app:register ai_image_generator_bot docker_dev --force-scopes \
+		--info-xml https://raw.githubusercontent.com/cloud-py-api/ai_image_generator_bot/main/appinfo/info.xml
+
+.PHONY: register
+register:
+	docker exec master-nextcloud-1 sudo -u www-data php occ app_api:app:unregister ai_image_generator_bot --silent || true
+	docker exec master-nextcloud-1 sudo -u www-data php occ app_api:app:register ai_image_generator_bot manual_install --json-info \
+  "{\"appid\":\"ai_image_generator_bot\",\"name\":\"AIImageGeneratorBot\",\"daemon_config_name\":\"manual_install\",\"version\":\"1.0.0\",\"secret\":\"12345\",\"host\":\"host.docker.internal\",\"port\":9080,\"scopes\":{\"required\":[\"TALK\", \"TALK_BOT\", \"FILES\", \"FILES_SHARING\"],\"optional\":[]},\"protocol\":\"http\",\"system_app\":1}" \
+  --force-scopes --wait-finish
+
+.PHONY: register27
+register27:
+	docker exec master-stable27-1 sudo -u www-data php occ app_api:app:unregister ai_image_generator_bot --silent || true
+	docker exec master-stable27-1 sudo -u www-data php occ app_api:app:register ai_image_generator_bot manual_install --json-info \
+  "{\"appid\":\"ai_image_generator_bot\",\"name\":\"AIImageGeneratorBot\",\"daemon_config_name\":\"manual_install\",\"version\":\"1.0.0\",\"secret\":\"12345\",\"host\":\"host.docker.internal\",\"port\":9080,\"scopes\":{\"required\":[\"TALK\", \"TALK_BOT\", \"FILES\", \"FILES_SHARING\"],\"optional\":[]},\"protocol\":\"http\",\"system_app\":1}" \
+  --force-scopes --wait-finish
---
+++ lib/main.py
@@ -0,0 +1,142 @@
+"""Stable Diffusion for Nextcloud Talk"""
+
+import io
+import queue
+import re
+import threading
+from contextlib import asynccontextmanager
+from datetime import datetime
+from typing import Annotated
+from time import perf_counter
+
+import torch
+from diffusers import AutoPipelineForText2Image
+from fastapi import BackgroundTasks, Depends, FastAPI, Response
+from huggingface_hub import snapshot_download
+
+from nc_py_api import AsyncNextcloudApp, NextcloudApp
+from nc_py_api.ex_app import (
+    LogLvl,
+    atalk_bot_app,
+    persistent_storage,
+    run_app,
+    set_handlers,
+)
+from nc_py_api.talk_bot import AsyncTalkBot, TalkBotMessage
+
+MODEL_NAME = "stabilityai/sdxl-turbo"
+MODEL_RUNTIME_OPT = {
+    "torch_dtype": torch.float32,
+    "variant": "fp32",
+}
+
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    if torch.cuda.is_available() or torch.backends.mps.is_available():
+        allow_patterns = ["*fp16.safetensors", "*.json", "*.txt"]
+        ignore_patterns = None
+        MODEL_RUNTIME_OPT["torch_dtype"] = torch.float16
+        MODEL_RUNTIME_OPT["variant"] = "fp16"
+    else:
+        allow_patterns = None
+        ignore_patterns = ["*onnx*", "*fp16*"]
+    set_handlers(
+        APP,
+        enabled_handler,
+        models_to_fetch={
+            MODEL_NAME: {
+                "allow_patterns": allow_patterns,
+                "ignore_patterns": ignore_patterns,
+            }
+        },
+    )
+    t = BackgroundProcessTask()
+    t.start()
+    yield
+
+
+APP = FastAPI(lifespan=lifespan)
+SD_BOT = AsyncTalkBot(
+    "/stable_diffusion", "Stable Diffusion", "@image cinematic shot of black pug wearing italian priest robe."
+)
+TASK_LIST = queue.Queue(maxsize=1)
+PIPE = None
+
+
+class BackgroundProcessTask(threading.Thread):
+    def run(self, *args, **kwargs):
+        global PIPE
+
+        while True:
+            try:
+                task: TalkBotMessage = TASK_LIST.get(block=True, timeout=60 * 60)
+                if PIPE is None:
+                    print("loading model")
+                    time_start = perf_counter()
+                    PIPE = AutoPipelineForText2Image.from_pretrained(
+                        snapshot_download(MODEL_NAME, local_files_only=True, cache_dir=persistent_storage()),
+                        **MODEL_RUNTIME_OPT,
+                    )
+                    if torch.cuda.is_available():
+                        PIPE.to("cuda")
+                    elif torch.backends.mps.is_available():
+                        PIPE.to("mps")
+                    print(f"model loaded: {perf_counter() - time_start}s")
+
+                r = re.search(r"@image\s(.*)", task.object_content["message"], re.IGNORECASE)
+                print("generating image")
+                time_start = perf_counter()
+                im = PIPE(prompt=r.group(1), num_inference_steps=1, guidance_scale=0.0).images[0]
+                print(f"image generated: {perf_counter() - time_start}s")
+                nc = NextcloudApp()
+                nc.set_user(task.actor_id[len("users/") :])
+                nc.files.makedirs("image_generator_bot", exist_ok=True)
+                im_out = io.BytesIO()
+                im.save(im_out, format="PNG")
+                im_out.seek(0)
+                new_im = nc.files.upload_stream(
+                    f"image_generator_bot/{datetime.now().strftime('%Y-%m-%d-%H-%M-%S.%f')}.png", im_out
+                )
+                nc.talk.send_file(new_im, task.conversation_token)
+            except queue.Empty:
+                if PIPE:
+                    print("offloading model")
+                PIPE = None
+            except Exception as e:  # noqa
+                print(str(e))
+                NextcloudApp().log(LogLvl.ERROR, str(e))
+
+
+async def stable_diffusion_process_request(message: TalkBotMessage):
+    try:
+        TASK_LIST.put(message, block=False)
+        print("task added")
+    except queue.Full:
+        await SD_BOT.send_message("*Task queue is full*", message)
+        print("queue full")
+
+
+@APP.post("/stable_diffusion")
+async def stable_diffusion(
+    message: Annotated[TalkBotMessage, Depends(atalk_bot_app)],
+    background_tasks: BackgroundTasks,
+):
+    if message.object_name == "message" and message.actor_id.startswith("users/"):
+        r = re.search(r"@image\s(.*)", message.object_content["message"], re.IGNORECASE)
+        if r is not None:
+            background_tasks.add_task(stable_diffusion_process_request, message)
+    return Response()
+
+
+async def enabled_handler(enabled: bool, nc: AsyncNextcloudApp) -> str:
+    print(f"enabled={enabled}")
+    try:
+        await SD_BOT.enabled_handler(enabled, nc)
+    except Exception as e:
+        return str(e)
+    return ""
+
+
+if __name__ == "__main__":
+    run_app("main:APP", log_level="trace")
